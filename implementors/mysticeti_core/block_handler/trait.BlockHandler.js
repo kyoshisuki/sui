c28855aced380cce71a72d6465b045424c237e61
ebmifa <ebmifa@users.noreply.github.com>
2023-11-04 19:41:41 +0000

    deploy: 83298e9a7c25e7c3c49d813caf199fce2ee3b06d

---
+++ implementors/mysticeti_core/block_handler/trait.BlockHandler.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"sui_core":[["impl BlockHandler for <a class=\"struct\" href=\"sui_core/mysticeti_adapter/struct.SimpleBlockHandler.html\" title=\"struct sui_core::mysticeti_adapter::SimpleBlockHandler\">SimpleBlockHandler</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
